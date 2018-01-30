3932ecedae60e6e5cf5042e425f87c1abba42c5f
php 09 <php.09@tom.cec.ime.usp.br>
2018-01-30 21:43:53 -0200

    Primeiro projeto

---
+++ resources/views/saudacao.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    @for ($i=0; $i < 100; $i++)
+        <h1>Olá {{$i+1}}  </h1>
+    @endfor
+
+  </body>
+</html>
