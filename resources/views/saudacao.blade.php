787575becd5065ed1d7a327088d37d53ee9e75a6
php 09 <php.09@tom.cec.ime.usp.br>
2018-01-30 21:53:09 -0200

    Primeira alteração

--- resources/views/saudacao.blade.php
+++ resources/views/saudacao.blade.php
@@ -2,12 +2,17 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Primeira Página :D</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
     @for ($i=0; $i < 100; $i++)
+      <div class="container">
         <h1>Olá {{$i+1}}  </h1>
+      </div>
     @endfor
+    <script type="text/javascript" scr="/js/app.js">
 
+    </script>
   </body>
 </html>
